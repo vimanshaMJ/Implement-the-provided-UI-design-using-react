ce18b3a53e1baa7f05cc6578df2a373762656c4f
vimansha <vimanshamadushani88@gmail.com>
2024-01-16 20:12:53 +0530

    style: accordion

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -1,39 +1,46 @@
 import React from "react";
 import Accordion from "react-bootstrap/Accordion";
+import classes from "./Accordion.module.css";
 
 export default function TheAccordion() {
   return (
     <div>
       <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>
+        <Accordion.Item eventKey="0" className={classes.accordionItem}>
+          <Accordion.Header className={classes.accordionHeader}>
             What Are The Taxes On Buy And Sell?
           </Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className={classes.accordionBody}>
             X Market Marker offers a comprehensive set of features tailored for
             market participants. Its core functionalities include real-time
             pricing, liquid provision and order book management.
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>How Is XMM Different From Others?</Accordion.Header>
-          <Accordion.Body>
+        <Accordion.Item eventKey="1" className={classes.accordionItem}>
+          <Accordion.Header className={classes.accordionHeader}>
+            How Is XMM Different From Others?
+          </Accordion.Header>
+          <Accordion.Body className={classes.accordionBody}>
             X Market Marker offers a comprehensive set of features tailored for
             market participants. Its core functionalities include real-time
             pricing, liquid provision and order book management.
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="2">
-          <Accordion.Header>How Can I Buy?</Accordion.Header>
-          <Accordion.Body>
+        <Accordion.Item eventKey="2" className={classes.accordionItem}>
+          <Accordion.Header className={classes.accordionHeader}>
+            How Can I Buy?
+          </Accordion.Header>
+          <Accordion.Body className={classes.accordionBody}>
             X Market Marker offers a comprehensive set of features tailored for
             market participants. Its core functionalities include real-time
             pricing, liquid provision and order book management.
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="3">
-          <Accordion.Header>How Do I Invest In Your Company?</Accordion.Header>
-          <Accordion.Body>
+        <Accordion.Item eventKey="3" className={classes.accordionItem}>
+          <Accordion.Header className={classes.accordionHeader}>
+            How Do I Invest In Your Company?
+          </Accordion.Header>
+          <Accordion.Body className={classes.accordionBody}>
             X Market Marker offers a comprehensive set of features tailored for
             market participants. Its core functionalities include real-time
             pricing, liquid provision and order book management.
